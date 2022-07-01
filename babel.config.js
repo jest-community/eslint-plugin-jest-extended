15b73367f66c6a2be70b1a2c126b8349a69041e6
Gareth Jones <jones258@gmail.com>
2022-07-02 10:07:55 +1200

    chore: misc config updates and changes

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const semver = require('semver');
+const pkg = require('./package.json');
+
+const supportedNodeVersion = semver.minVersion(pkg.engines.node).version;
+
 // todo: https://github.com/babel/babel/issues/8529 :'(
 module.exports = {
   plugins: ['replace-ts-export-assignment'],
   presets: [
     '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 8 } }],
+    ['@babel/preset-env', { targets: { node: supportedNodeVersion } }],
   ],
 };
