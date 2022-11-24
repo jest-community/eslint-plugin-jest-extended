4336db9981502d87516d8ab7b947d3a814381a0a
Gareth Jones <jones258@gmail.com>
2022-11-25 11:04:24 +1300

    ci: setup weekly smoke test

---
+++ eslint-remote-tester.config.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import type { Config } from 'eslint-remote-tester';
+import {
+  getPathIgnorePattern,
+  getRepositories,
+} from 'eslint-remote-tester-repositories';
+
+const config: Config = {
+  repositories: getRepositories({ randomize: true }),
+  pathIgnorePattern: getPathIgnorePattern(),
+  extensions: ['js', 'jsx', 'ts', 'tsx'],
+  concurrentTasks: 3,
+  cache: false,
+  logLevel: 'info',
+  eslintrc: {
+    root: true,
+    env: {
+      es6: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    extends: ['plugin:jest-extended/all'],
+    rules: {},
+  },
+};
+
+export default config;
